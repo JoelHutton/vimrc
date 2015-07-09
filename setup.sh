b4d18b569c5a2ab5a5a546cfafc7f235985b1c6f
Joel Hutton <joel.hutton@arm.com>
2015-07-09 11:53:26 +0100

    changed setup to have option for do globally

--- setup.sh
+++ setup.sh
@@ -1,41 +1,54 @@
 #!/bin/bash
+GLOBAL=false;
 
+#iterate through cli arguments
+for var in "$@"
+do
+	case $var in
+		"--global")
+			GLOBAL= true
+			;;
+		*)
+			echo "unrecognised flag $var"
+			exit 1
+			;;
+	esac
+done
 if [[ $USER == "root" ]]; then 
 	echo "This script must not be run as root" 
 	exit 1
 fi 
 
+#backup file, unless backup already present, in which case return 1
+backup(){
+	filename= "$1"
+	backup_filename= "$2"
+	if [ ! -f $backup_filename ]; then 
+		if [ -f $filename ]; then 
+			sudo mv $filename $backup_filename
+		fi
+	else
+		exit 1
+	fi
+	
+}
+
 if [ -f ./vimrc ]
 then
 	echo "backing up current vimrc's in .bak.name"
-	if [ ! -f ~/.bak.vimrc ]; then 
-		if [ -f ~/.vimrc ]; then 
-			mv ~/.vimrc ~/.bak.vimrc
-		fi
-	else
-		echo "backups already exist, exitting"
-		exit 1
-	fi
-	if [ ! -f /etc/vim/.bak.vimrc ]; then 
-		if [ -f /etc/vim/vimrc ]; then 
-			sudo mv /etc/vim/vimrc /etc/vim/.bak.vimrc
-		fi
-	else
-		echo "backups already exist, exitting"
-		exit 1
-	fi
-	if [ ! -f /root/.bak.vimrc ]; then 
-		if [ -f /root/vimrc ]; then 
-			sudo mv /root/vimrc /root/.bak.vimrc
-		fi
-	else
-		echo "backups already exist, exitting"
-		exit 1
+	backup /home/$USER/.vimrc /home/$USER/.bak.vimrc
+	if [[ $GLOBAL ]]
+	then
+		backup /etc/vim/vimrc /etc/vim/.bak.vimrc
+		backup /root/vimrc /root/.bak.vimrc
 	fi
 	echo "copying files into place"
-	sudo ln -s $(pwd)/vimrc ~/.vimrc
-	sudo ln -s $(pwd)/vimrc /etc/vim/vimrc
-	sudo ln -s $(pwd)/vimrc /root/vimrc
+	ln -s $(pwd)/vimrc ~/.vimrc
+	if [[ $GLOBAL ]]
+	then
+		sudo ln -s $(pwd)/vimrc /etc/vim/vimrc
+		sudo ln -s $(pwd)/vimrc /root/vimrc
+	fi
 else 
 	echo "please execute from within the directory (can't find vimrc)"
 fi
